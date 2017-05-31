75e60e9d5334e21d0f5ca2d94a75093d1ed66acb
Dio Phung <dio.phung@xero.com>
2017-05-30 23:41:38 -0700

    Add ERD

--- Acme.Billing.Database/Report.sql
+++ Acme.Billing.Database/Report.sql
@@ -1,7 +1,6 @@
 ﻿CREATE TABLE [dbo].[Report]
 (
     [RowId] BIGINT NOT NULL PRIMARY KEY, 
-    [CustomerId] NCHAR(255) NOT NULL, 
     [Content] NVARCHAR(MAX) NOT NULL, 
     [Month] INT NOT NULL, 
     [Year] INT NOT NULL, 
